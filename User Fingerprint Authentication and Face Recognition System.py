eeb7551a030b8263841185a739fa19112208f477
RAJ KUMAR A <146355426+RAJ322622@users.noreply.github.com>
2025-04-15 09:31:55 +0530

    Update User Fingerprint Authentication and Face Recognition System.py

--- User Fingerprint Authentication and Face Recognition System.py
+++ User Fingerprint Authentication and Face Recognition System.py
@@ -76,7 +76,7 @@ class FaceAuth:
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
         rgb_small_frame = small_frame[:, :, ::-1]
         
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_locations = face_locations = [(top*4, right*4, bottom*4, left*4) for top, right, bottom, left in face_locations]
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         
